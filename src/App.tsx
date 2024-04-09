937829c208a81b8b89fb21a319b7c1554b685c71
Humberto Rivero <humbertorivero9@gmail.com>
2024-04-09 14:22:49 -0300

    fix: grid posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import {useEffect, useState} from "react";
 import {Post} from "./interface/post";
 import {getNextPage} from "./services/getNextPage";
 import Spinner from "./components/Spinner";
-import { createPost } from "./services/createPost";
+import {createPost} from "./services/createPost";
 
 function App() {
 	const [posts, setPosts] = useState<Post[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [hasError, setHasError] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
-  const [reachedLastPage, setReachedLastPage] = useState(false);
+	const [reachedLastPage, setReachedLastPage] = useState(false);
 
 	const fetchPosts = async (page: number) => {
 		setIsLoading(true);
@@ -36,28 +36,27 @@ function App() {
 
 	const prevPage = () => {
 		setCurrentPage((prevPage) => prevPage - 1);
-  };
-  
-  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	};
 
-    const formData = new FormData(e.currentTarget);
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    const title = formData.get('title') as string;
-    const body = formData.get('body') as string;
+		const formData = new FormData(e.currentTarget);
 
-    const newPost = {
-      title,
-      body
-    }
+		const title = formData.get("title") as string;
+		const body = formData.get("body") as string;
 
-    const response = await createPost(newPost);
+		const newPost = {
+			title,
+			body,
+		};
 
-    if (response) {
-      setPosts([...posts, response]);
-    }
+		const response = await createPost(newPost);
 
-  };
+		if (response) {
+			setPosts([...posts, response]);
+		}
+	};
 
 	useEffect(() => {
 		fetchPosts(currentPage);
@@ -79,7 +78,10 @@ function App() {
 				<div className='grid grid-cols-5 gap-6'>
 					<section className='col-span-2'>
 						<h2>New Post</h2>
-						<form onSubmit={handleSubmit} className='bg-white shadow-md rounded-md p-4'>
+						<form
+							onSubmit={handleSubmit}
+							className='bg-white shadow-md rounded-md p-4'
+						>
 							<div className='mb-4'>
 								<label
 									htmlFor='title'
@@ -118,13 +120,13 @@ function App() {
 					</section>
 					<section className='col-span-3'>
 						<h2>Posts</h2>
-						<div className='min-h-[700px]'>
+						<div>
 							{isLoading ? (
 								<div className='flex items-center justify-center'>
 									<Spinner />
 								</div>
 							) : (
-								<div className='grid grid-cols-1 grid-rows-2 sm:grid-cols-2 lg:grid-cols-2 gap-8'>
+								<div className='grid grid-cols-1 grid-rows-2 sm:grid-cols-2 lg:grid-cols-2 gap-8 min-h-80'>
 									{posts.map((post) => (
 										<div
 											key={post.id}
@@ -141,24 +143,24 @@ function App() {
 							{posts.length === 0 && (
 								<p className='text-center text-gray-600'>No posts available</p>
 							)}
+							<div className='flex justify-center mt-8'>
+								<button
+									onClick={prevPage}
+									disabled={currentPage === 1}
+									className='bg-blue-500 text-white font-semibold px-4 py-2 mr-2 rounded disabled:bg-gray-300'
+								>
+									Previous
+								</button>
+								<button
+									onClick={nextPage}
+									disabled={reachedLastPage || isLoading || posts.length === 0}
+									className='bg-blue-500 text-white font-semibold px-4 py-2 rounded disabled:bg-gray-300'
+								>
+									Next
+								</button>
+							</div>
 						</div>
 					</section>
-				</div>
-				<div className='flex justify-center mt-8'>
-					<button
-						onClick={prevPage}
-						disabled={currentPage === 1}
-						className='bg-blue-500 text-white font-semibold px-4 py-2 mr-2 rounded disabled:bg-gray-300'
-					>
-						Previous
-					</button>
-					<button
-						onClick={nextPage}
-						disabled={reachedLastPage || isLoading || posts.length === 0}
-						className='bg-blue-500 text-white font-semibold px-4 py-2 rounded disabled:bg-gray-300'
-					>
-						Next
-					</button>
 				</div>
 			</main>
 		</div>
