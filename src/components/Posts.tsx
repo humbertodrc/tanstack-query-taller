e78b0aad1f68ff273e12a2bce36899f36b4705fa
Humberto Rivero <humbertorivero9@gmail.com>
2024-04-09 17:52:29 -0300

    add: method useQuery

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,19 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { getNextPage } from "../services/getNextPage";
+import {useQuery} from "@tanstack/react-query";
+import {useState} from "react";
+import {getNextPage} from "../services/getNextPage";
 import Spinner from "./Spinner";
 import NoAviable from "./NoAviable";
-import Post from './Post';
+import Post from "./Post";
 
 export default function Posts() {
-  const [currentPage, setCurrentPage] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
 
-  const query = useQuery({
-    queryKey: ["posts", currentPage],
-    queryFn: () => getNextPage(currentPage, 4),
-  })
-  
-  const nextPage = () => {
+	const query = useQuery({
+		queryKey: ["posts", currentPage],
+		queryFn: () => getNextPage(currentPage, 4),
+		// refetchOnWindowFocus: false,
+		// refetchInterval: 2000,
+		// staleTime: 60000,
+		// staleTime: Infinity,
+		// gcTime: 3000,
+		// retry: 1
+	});
+
+	const nextPage = () => {
 		setCurrentPage((prevPage) => prevPage + 1);
 	};
 
@@ -23,7 +29,7 @@ export default function Posts() {
 
 	return (
 		<section className='col-span-3'>
-			<h2>Posts</h2>
+			<h2>Posts {query.isFetching && <Spinner />}</h2>
 			<div>
 				{query.isLoading ? (
 					<div className='flex items-center justify-center'>
@@ -31,9 +37,8 @@ export default function Posts() {
 					</div>
 				) : (
 					<div className='grid grid-cols-1 grid-rows-2 sm:grid-cols-2 lg:grid-cols-2 gap-8 min-h-80'>
-						{query.data && query.data.map((post) => (
-							<Post key={post.id} post={post} />
-						))}
+						{query.data &&
+							query.data.map((post) => <Post key={post.id} post={post} />)}
 					</div>
 				)}
 				{query.data?.length === 0 && <NoAviable />}
